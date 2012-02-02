adf14321b0315dd56831cebb6d0020648c9bb3d1
Aaron Price <aaron@29degrees.co.uk>
2012-02-02 11:50:14 -0400

    Add the sessions table to the schema.

--- db/schema.sql
+++ db/schema.sql
@@ -79,6 +79,25 @@ CREATE TABLE `comments` (
 -- --------------------------------------------------------
 
 -- 
+-- Table structure for table `comments`
+--
+
+CREATE TABLE IF NOT EXISTS  `ci_sessions` (
+  `session_id` varchar(40) DEFAULT '0' NOT NULL,
+  `ip_address` varchar(16) DEFAULT '0' NOT NULL,
+  `user_agent` varchar(50) NOT NULL,
+  `last_activity` int(10) unsigned DEFAULT 0 NOT NULL,
+  `user_data` text NOT NULL,
+	PRIMARY KEY (`session_id`)
+);
+
+-- 
+-- Dumping data for table `comments`
+--
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `favorites`
 -- 
 
